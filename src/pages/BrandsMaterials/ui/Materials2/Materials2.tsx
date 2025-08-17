7ba593af66d737c2825973f6aaf220102a1e0916
Jumaniyaz <zumaniazkozubaev@gmail.com>
2025-08-17 18:50:12 +0600

    new file

--- src/pages/BrandsMaterials/ui/Materials2/Materials2.tsx
+++ src/pages/BrandsMaterials/ui/Materials2/Materials2.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Materials2.module.scss";
 import { Link } from "react-router-dom";
 
@@ -14,13 +14,28 @@ const materials: Material2[] = [
   { id: 2, image: "https://cdn-sh1.vigbo.com/shops/184868/products/22341008/images/2-e14428d4acf615f74cb36aaa67e09f87.jpg", name: 'Название мерча 2', price: "1200 KGZ" },
   { id: 3, image: "https://cdn-sh1.vigbo.com/shops/184868/products/22341008/images/2-e14428d4acf615f74cb36aaa67e09f87.jpg", name: 'Название мерча 3', price: "1500 KGZ" },
   { id: 4, image: "https://cdn-sh1.vigbo.com/shops/184868/products/22341008/images/2-e14428d4acf615f74cb36aaa67e09f87.jpg", name: 'Название мерча 4', price: "900 KGZ" },
-  { id: 5, image: "https://cdn-sh1.vigbo.com/shops/184868/products/22341008/images/2-e14428d4acf615f74cb36aaa67e09f87.jpg", name: 'Название мерча 5 ', price: "1100 KGZ" },
+  { id: 5, image: "https://cdn-sh1.vigbo.com/shops/184868/products/22341008/images/2-e14428d4acf615f74cb36aaa67e09f87.jpg", name: 'Название мерча 5', price: "1100 KGZ" },
   { id: 6, image: "https://cdn-sh1.vigbo.com/shops/184868/products/22341008/images/2-e14428d4acf615f74cb36aaa67e09f87.jpg", name: 'Название мерча 6', price: "1300 KGZ" },
 ];
 
 const Materials2: React.FC = () => {
   const [startIndex, setStartIndex] = useState(0);
-  const visibleCount = 4;
+  const [visibleCount, setVisibleCount] = useState(4);
+
+  const updateVisibleCount = () => {
+    const width = window.innerWidth;
+    if (width <= 600) setVisibleCount(1);
+    else if (width <= 900) setVisibleCount(2);
+    else if (width <= 1200) setVisibleCount(3);
+    else setVisibleCount(4);
+  };
+
+  useEffect(() => {
+    updateVisibleCount();
+    window.addEventListener("resize", updateVisibleCount);
+    return () => window.removeEventListener("resize", updateVisibleCount);
+  }, []);
+
   const maxIndex = materials.length - visibleCount;
 
   const handlePrev = () => {
@@ -46,7 +61,6 @@ const Materials2: React.FC = () => {
               <p className={styles.price}>{item.price}</p>
             </div>
           </Link>
-
         ))}
       </div>
 
@@ -56,22 +70,21 @@ const Materials2: React.FC = () => {
         </button>
 
         <div className={styles.pagination}>
-          {[1, 2, 3].map((num) => (
+          {Array.from({ length: Math.ceil(materials.length / visibleCount) }, (_, i) => (
             <button
-              key={num}
-              className={startIndex === num - 1 ? styles.activeNumber : ""}
-              onClick={() => handleNumberClick(num)}
+              key={i}
+              className={startIndex === i * visibleCount ? styles.activeNumber : ""}
+              onClick={() => setStartIndex(i * visibleCount)}
             >
-              {num}
+              {i + 1}
             </button>
           ))}
         </div>
 
-        <button className={styles.btns} onClick={handleNext} disabled={startIndex === maxIndex}>
+        <button className={styles.btns} onClick={handleNext} disabled={startIndex >= maxIndex}>
           <img src="https://cdn-icons-png.flaticon.com/512/32/32213.png" alt="next" />
         </button>
       </div>
-
     </div>
   );
 };
