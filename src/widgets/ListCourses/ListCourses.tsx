9e36d5f49f57c2fc4a6c9471231b79bd4fbbd959
BAKYTq1 <bsainaev@gmail.com>
2025-10-11 22:07:21 +0600

    ema

---
+++ src/widgets/ListCourses/ListCourses.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './ListCourses.module.scss';
+import dropDownImg from '../../shared/assets/images/drop down.svg';
+import instagramImg from '../../shared/assets/images/instagram-line.svg';
+import telegramImg from '../../shared/assets/images/telegram-2-fill.svg';
+import img from '../../shared/assets/icons/Arrow.svg'
+import img1 from '../../shared/assets/images/instagram-line.svg'
+import img2 from '../../shared/assets/images/telegram-2-fill.svg'
+interface ListCoursesCardProps {
+  title: string;
+  description: string;
+  imageSrc: string; 
+  bgColor: string;
+  onClick?: () => void;
+}
+
+const ListCourses: React.FC<ListCoursesCardProps> = ({ title, description, imageSrc, bgColor, onClick }) => {
+  const cardStyle = {
+    '--background-image-url': `url(${imageSrc})`,
+    boxSizing: 'border-box',
+    background: `linear-gradient(to left, rgba(0, 0, 0, 0) 0%, ${bgColor} 43%), var(--background-image-url) no-repeat right bottom / auto 100%`,
+  } as React.CSSProperties;
+    
+  return (
+    <div className={clsx(styles.activityCard)} style={cardStyle}>
+      <div className={styles.content}>
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.description}>{description}...</p>
+        <div className={styles.buttons}>
+        <button onClick={onClick} className={styles.button}>Записаться <img src={img} alt="" /></button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListCourses;
